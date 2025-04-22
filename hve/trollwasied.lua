034a3b5563e7928ad9382ec7e32773d7823251ee
YohSambre <YohSambre@users.noreply.github.com>
2025-04-23 00:50:29 +0200

    Update trollwasied.lua

--- hve/trollwasied.lua
+++ hve/trollwasied.lua
@@ -4,7 +4,6 @@ local function WasiedSoGood()
     chat.AddText(Color(math.random(0, 255), math.random(0, 255), math.random(0, 255)), "LEARN GLUA EASILY > https://formations.wasied.com")
     notification.AddLegacy("LEARN GLUA EASILY > https://formations.wasied.com", NOTIFY_HINT, 5)
 end
-
 hook.Add("Think", "WasiedSexyGuy", WasiedSoGood)
 
 hook.Add("StartChat", "OkPlayerCopymylinkforopeninyourbrowser", function()
